2514271f16f4404435d87fe6497296f4cee19d72
JakubGryzioHex <86599920+JakubGryzioHex@users.noreply.github.com>
2022-12-21 12:30:06 +0100

    Created employee model

---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dotnet_api.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? LastName { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public float Salary { get; set; }
+
+        public bool IsDeveloper { get; set; }
+
+        public DateTime StartWorkingDate { get; set; }
+
+    }
+}
